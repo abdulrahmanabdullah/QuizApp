9bcf8e73f9f1391f9b84f38235ea1883ef7ddb60
abdulrahman-abdullah <nfs056@gmail.com>
2017-11-28 02:16:28 +0300

    check answer in SecondActivity and send student name

--- app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/MainActivity.java
+++ app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/MainActivity.java
@@ -8,6 +8,7 @@ import android.transition.Explode;
 import android.transition.Slide;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,14 +26,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar_layout);
+        tx = findViewById(R.id.txv_name);
+        setTx();
+        Toast.makeText(this,tx.getText().toString(),Toast.LENGTH_LONG).show();
         setSupportActionBar(toolbar);
         toolbar.setTitle(getString(R.string.app_name));
        setUpAnim();
-       receiveStudentName();
     }
 
     /**
      * menu methods ..
+     * @param menu  set menu file layout .
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     *
+     * @param item setting, id = setting .
+     * @return true
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
@@ -52,15 +61,28 @@ public class MainActivity extends AppCompatActivity {
         return true ;
     }
 
+    /**
+     * Appear MainActivity with Explode animations .
+     */
     public void setUpAnim(){
         Explode explode = new Explode();
         explode.setDuration(1000);
         getWindow().setEnterTransition(explode);
     }
 
-   public void receiveStudentName(){
-       tx = findViewById(R.id.txv_name);
-       String str =  getIntent().getStringExtra(ConstantValues.NAME);
-       tx.setText(str);
+    public void setTx(){
+        String str = getIntent().getStringExtra(ConstantValues.NAME).toString() ;
+        tx.setText(str);
+    }
+   public String receiveStudentName(){
+       String str =  tx.getText().toString();
+       return  str ;
    }
+
+  public void goToNextSlide(View v){
+       Intent intent = new Intent(this,SecondQuestion.class);
+       //TODO:  put student name .
+      intent.putExtra(ConstantValues.NAME,receiveStudentName());
+      startActivity(intent);
+  }
 }
--- app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/SecondQuestion.java
+++ app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/SecondQuestion.java
@@ -1,17 +1,88 @@
 package abdulrahmanjavanrd.com.quizapp_project3;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.icu.text.UnicodeSetSpanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import abdulrahmanjavanrd.com.quizapp_project3.constant.ConstantValues;
 
 public class SecondQuestion extends AppCompatActivity {
 
+    EditText etAnswer7 ;
+    CheckBox yesAnswer , noAnswer ;
+    int calcuScore ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_question);
+        Toolbar toolbar = findViewById(R.id.second_toolBar);
+        etAnswer7 = findViewById(R.id.et_answer_7);
+        yesAnswer = findViewById(R.id.checkBox_yes);
+        noAnswer = findViewById(R.id.checkBox_no);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        toolbar.setSubtitle(getString(R.string.complete_question));
     }
 
+    /**
+     *
+     * @param str to check what the student wrote.
+     * @return string  what the etAnswer is .
+     */
+    public String checkAnswer7(String str){
+        if (str.equalsIgnoreCase("jvm")){
+            etAnswer7.setBackgroundColor(Color.GREEN);
+           calcuScore++;
+        }else {
+            etAnswer7.setBackgroundColor(Color.RED);
+        }
+        return str ;
+    }
     public void showQuizResult(View view) {
+        String str = etAnswer7.getText().toString() ;
+        checkAnswer7(str);
+        if (yesAnswer.isChecked()){
+            yesAnswer.setBackgroundColor(Color.RED);
+            noAnswer.setChecked(true);
+            noAnswer.setBackgroundColor(Color.GREEN);
+        }
+        if(noAnswer.isChecked()){
+           noAnswer.setBackgroundColor(Color.GREEN);
+        }
+        // TODO: call Dialog here .
+        Toast.makeText(this," correct  = " + calcuScore,Toast.LENGTH_LONG).show();
+        getPreviouslyAnswer();
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main_menu,menu);
+//        return  true ;
+//    }
+//
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.setting :
+//                Toast.makeText(this,"settings clicked ",Toast.LENGTH_LONG).show();
+//                break;
+//            default:
+//                break;
+//        }
+//        return true ;
+//    }
+
+    public void getPreviouslyAnswer(){
+        // First get student name .
+        String studentName = getIntent().getStringExtra(ConstantValues.NAME);
+        Toast.makeText(this," student name = " + studentName, Toast.LENGTH_LONG).show();
     }
 }
