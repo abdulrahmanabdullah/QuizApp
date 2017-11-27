83c84cfc8f8f0f8565e59abe21005bff14e1684f
abdulrahman-abdullah <nfs056@gmail.com>
2017-11-28 02:28:42 +0300

    write some documentation

--- app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/MainActivity.java
+++ app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/MainActivity.java
@@ -5,11 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
-import android.transition.Slide;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,17 +18,19 @@ public class MainActivity extends AppCompatActivity {
      *  pure app for copy and past in future .
      * @param savedInstanceState
      */
-    TextView tx  ;
+    TextView txvStudentName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar_layout);
-        tx = findViewById(R.id.txv_name);
-        setTx();
-        Toast.makeText(this,tx.getText().toString(),Toast.LENGTH_LONG).show();
         setSupportActionBar(toolbar);
         toolbar.setTitle(getString(R.string.app_name));
+        txvStudentName = findViewById(R.id.txv_name);
+        /** call {@link #setTxvStudentName()}  method  to set student name */
+        setTxvStudentName();
+        Toast.makeText(this, txvStudentName.getText().toString(),Toast.LENGTH_LONG).show();
+        /**  call {@link #setUpAnim()} for  animations */
        setUpAnim();
     }
 
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *
      * @param item setting, id = setting .
      * @return true
      */
@@ -70,18 +69,29 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setEnterTransition(explode);
     }
 
-    public void setTx(){
-        String str = getIntent().getStringExtra(ConstantValues.NAME).toString() ;
-        tx.setText(str);
+    /**
+     * First call the student name from Splash screen
+     * second set the student name in textView <p>This TextView gone Visibility</p>
+     */
+    public void setTxvStudentName(){
+        String str = getIntent().getStringExtra(ConstantValues.NAME) ;
+        txvStudentName.setText(str);
     }
+
+    /**
+     * @return The Text in TextView .
+     */
    public String receiveStudentName(){
-       String str =  tx.getText().toString();
+       String str =  txvStudentName.getText().toString();
        return  str ;
    }
 
+    /**
+     * @param v button on main_app_layout , This Button send all answers in this <p>MainActivity</p> Page And Student Name .
+     *          Inside this method i called {@link #receiveStudentName()} to get Student Name .
+     */
   public void goToNextSlide(View v){
        Intent intent = new Intent(this,SecondQuestion.class);
-       //TODO:  put student name .
       intent.putExtra(ConstantValues.NAME,receiveStudentName());
       startActivity(intent);
   }
