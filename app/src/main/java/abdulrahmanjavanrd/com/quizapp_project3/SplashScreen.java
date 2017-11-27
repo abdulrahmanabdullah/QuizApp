92de2342f44043e17fb93b914a6471b8fcf22ced
abdulrahman-abdullah <nfs056@gmail.com>
2017-11-27 11:28:07 +0300

    create transition for splash screen

--- app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/SplashScreen.java
+++ app/src/main/java/abdulrahmanjavanrd/com/quizapp_project3/SplashScreen.java
@@ -1,5 +1,6 @@
 package abdulrahmanjavanrd.com.quizapp_project3;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,17 +10,25 @@ import android.transition.TransitionManager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import abdulrahmanjavanrd.com.quizapp_project3.constant.ConstantValues;
 
 public class SplashScreen extends AppCompatActivity {
 
     /**
-     * @a
-     * {@link #studentName} EditText to take student name.
+     *
+     * {@link #studentName} id = et_student_name ;
+     * {@link #upLinear} This LinearLayout contain imageView and TextView, Position Top parent .
+     *  {@link #downLinear } LinearLaout contain Button , textView . Position Bottom parent .
      */
     EditText studentName ;
+    LinearLayout upLinear , downLinear  ;
+
+    Animation upAni ,downAni;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +36,13 @@ public class SplashScreen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         studentName = findViewById(R.id.et_student_name);
+        upLinear = findViewById(R.id.up_linear);
+        downLinear = findViewById(R.id.down_linear);
+        upAni = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downAni = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        upLinear.setAnimation(upAni);
+        downLinear.setAnimation(downAni);
+        studentName.setAnimation(downAni);
     }
     /**
      *
@@ -37,12 +53,13 @@ public class SplashScreen extends AppCompatActivity {
      */
     public void callMainActivity(View view){
         String str = studentName.getText().toString() ;
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this);
         Intent  intent = new Intent(this,MainActivity.class);
         intent.putExtra(ConstantValues.NAME,str);
 //        Transition tran  = TransitionInflater.from(this).inflateTransition(R.transition.first_screen_transisiton);
 //        tran.setDuration(500);
 //        getWindow().setEnterTransition(tran);
-        startActivity(intent);
+        startActivity(intent,options.toBundle());
         this.finish();
     }
 }
